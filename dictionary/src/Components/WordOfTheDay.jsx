ec1c89868d7b33d536794c41c37f3b84c0e06bc8
Mufadal03 <sadrimufadal@gmail.com>
2022-12-16 18:40:33 +0530

    done : word of the day

--- dictionary/src/Components/WordOfTheDay.jsx
+++ dictionary/src/Components/WordOfTheDay.jsx
@@ -1,8 +1,32 @@
+import { Box, Flex, Heading, Text } from '@chakra-ui/react'
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
 
 const WordOfTheDay = () => {
+  const {Data,Loading,Error,ErrorMessage} = useSelector((state) => {
+        return {
+                Data: state.WordOfTheDay.data,
+                Loading: state.WordOfTheDay.Loading,
+                Error: state.WordOfTheDay.Error,
+                ErrorMessage:state.WordOfTheDay.ErrorMessage
+            }
+  })
+  if (Loading) {
+    return <h1>Loading</h1>
+  }
+  const singleData = Data?.find((item, i) => i == 1)
   return (
-    <div>WordOfTheDay</div>
+    <Flex direction={"column"} alignItems="center" gap="1rem" py='2rem'>
+      <Heading fontFamily={"cursive"} fontWeight="md">Word Of The Day</Heading>
+      <Flex direction={"column"} border="2px solid" gap="1rem" padding={"2rem"}>
+      <Text textAlign={"center"} fontSize="2xl" fontFamily={"cursive"}>{singleData?.date }</Text>
+        <Text fontSize="2xl" fontFamily={"cursive"}>Word :<Text textTransform={"uppercase"} as="span"> {singleData?.word}</Text></Text>
+        <Text fontSize="2xl" fontFamily={"cursive"}>Meaning : {singleData?.mean}</Text>
+        <Text fontSize="2xl" fontFamily={"cursive"}>Type : { singleData?.type}</Text>
+      </Flex>
+    </Flex>
   )
 }
 
