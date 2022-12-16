512410073f96511fa31d94c3d5dd477747e69a6a
Mufadal03 <sadrimufadal@gmail.com>
2022-12-16 18:42:21 +0530

    fetched correction

--- dictionary/src/Components/Word.jsx
+++ dictionary/src/Components/Word.jsx
@@ -1,6 +1,19 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 const Word = () => {
+    const {Data,Loading,Error,ErrorMessage} = useSelector((state) => {
+        return {
+                Data: state.SearchReducer.data,
+                Loading: state.SearchReducer.Loading,
+                Error: state.SearchReducer.Error,
+                ErrorMessage:state.SearchReducer.ErrorMessage
+            }
+    })
+    const { meanings, phonetics, word } = Data
+    console.log("MeaningsArray", meanings)
+    console.log("Phot", phonetics)
+    console.log("Word",word)
   return (
     <div>Word</div>
   )
