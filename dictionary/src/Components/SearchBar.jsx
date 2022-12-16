1b23f75ac6767b9d6c982e617f5cf79c635bd904
Mufadal03 <sadrimufadal@gmail.com>
2022-12-16 18:41:45 +0530

    added deouncing in searching

--- dictionary/src/Components/SearchBar.jsx
+++ dictionary/src/Components/SearchBar.jsx
@@ -1,10 +1,31 @@
 import { Box, Flex, Input } from '@chakra-ui/react'
 import React from 'react'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { getWord } from '../redux/SearchRedux/action'
 
 const SearchBar = () => {
+  const [text, setText] = useState("")
+  const dispatch = useDispatch()
+  const handleChange = (e) => {
+    const { value } = e.target
+    setText(value)
+  }
+  const handleApiCall = () => {
+    if(text=="")return
+    console.log(text)
+    dispatch(getWord(text))
+  }
+  useEffect(() => {
+    let running = setTimeout(() => {
+      handleApiCall()
+    }, 500)
+    return ()=>clearTimeout(running)
+  },[text])
   return (
       <Flex justifyContent={"center"}  p="2rem">
-          <Input placeholder="Search Word" type='search' borderRadius="none" _focus={{borderColor:"white"}} h="50px" fontSize={"2xl"} w="90%"/>
+          <Input value={text} onChange={(e)=>handleChange(e)} placeholder="Search Word" type='search' borderRadius="none" _focus={{borderColor:"white"}} h="50px" fontSize={"2xl"} w="90%"/>
     </Flex>
   )
 }
