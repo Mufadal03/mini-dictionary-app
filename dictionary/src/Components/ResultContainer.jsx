e96a44d09bc38df3edca02b766f6890f3c24a89e
Mufadal03 <sadrimufadal@gmail.com>
2022-12-16 17:33:13 +0530

    Result Container setup

---
+++ dictionary/src/Components/ResultContainer.jsx
@@ -0,0 +1,23 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+import { useSelector } from 'react-redux'
+import Word from './Word'
+import WordOfTheDay from './WordOfTheDay'
+
+const ResultContainer = () => {
+    const {Data,Loading,Error,ErrorMessage} = useSelector((state) => {
+        return {
+                Data: state.SearchReducer.data,
+                Loading: state.SearchReducer.Loading,
+                Error: state.SearchReducer.Error,
+                ErrorMessage:state.SearchReducer.ErrorMessage
+            }
+    })
+  return (
+      <Box border="2px solid" h="300px">
+          {Data.length>0?<Word />:<WordOfTheDay />}
+    </Box>
+  )
+}
+
+export default ResultContainer
