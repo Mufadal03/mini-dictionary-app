d7b55615d1a993a6d56171dc29cc867d585697f1
Mufadal03 <sadrimufadal@gmail.com>
2022-12-17 21:29:13 +0530

    minor touch up

--- dictionary/src/redux/SearchRedux/action.js
+++ dictionary/src/redux/SearchRedux/action.js
@@ -6,11 +6,9 @@ export const getWord = (word) => dispatch => {
     dispatch({ type: data.GET_WORD_REQUEST })
     axios.get(process.env.REACT_APP_SEARCH_URL+word)
         .then((r) => {
-            console.log(r.data[0])
             dispatch({ type: data.GET_WORD_SUCCESS, payload:r.data[0]})
         })
         .catch((e) => {
-            console.log(e)
         dispatch({type:data.GET_WORD_FAILURE,payload:e})
     })
 }
