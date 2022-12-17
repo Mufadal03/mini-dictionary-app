48d85a61951e8cccbbfd5d541b048e1b832136f1
Mufadal03 <sadrimufadal@gmail.com>
2022-12-17 16:55:02 +0530

    word comp

--- dictionary/src/Components/Word.jsx
+++ dictionary/src/Components/Word.jsx
@@ -1,9 +1,12 @@
-import { Box, Flex, Heading } from '@chakra-ui/react'
+import { Box, Flex, Heading, Icon } from '@chakra-ui/react'
 import React from 'react'
 import { useSelector } from 'react-redux'
 import Card from './Card'
-
+import { HiSpeakerWave } from "react-icons/hi"
+import { HiOutlineSearch } from "react-icons/hi"
+import {RxSpeakerLoud} from "react-icons/rx"
 const Word = () => {
+  let src 
     const {Data,Loading,Error,ErrorMessage} = useSelector((state) => {
         return {
                 Data: state.SearchReducer.data,
@@ -14,15 +17,21 @@ const Word = () => {
     })
     const { meanings, phonetics, word } = Data
   const Audio_src = phonetics.find((item) => item.audio != "")
-  // const { audio } = Audio_src
-  // console.log(audio)
+  if (Audio_src) {
+    const { audio } = Audio_src
+    src=audio
+  }
+  const handleClick = () => {
+    let sound = new Audio(src)
+    sound.play()
+  }
   return (
-    <Box border="2px solid" fontFamily={"cursive"}>
-      <Flex border="2px solid" alignItems={"center"} justifyContent="" py='1rem'>
+    <Box fontFamily={"cursive"}>
+      <Flex  alignItems={"center"} justifyContent="" py='1rem'>
         <Box flexGrow={"2"} textAlign="center"><Heading fontWeight={"md"} fontFamily="cursive">{word}</Heading></Box>
-        <Box flexGrow={".5"} textAlign="center">Sound Icon audio</Box>
+        <Box flexGrow={".5"} textAlign="center" >{ src && <Icon onClick={handleClick} cursor='pointer' h="6" w="6" as={RxSpeakerLoud} />}</Box>
       </Flex>
-      <Flex direction={"column"} gap=".5rem" border={"2px solid"} p="2rem">
+      <Flex direction={"column"} gap="1.5rem" p="2rem">
         {
         meanings.length > 0 && meanings.map((item,i) => (
           <Card key={i} {...item} />
