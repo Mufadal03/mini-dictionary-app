dbef0581e91837439b4b9fbd1d5e9739b1156657
Mufadal03 <sadrimufadal@gmail.com>
2022-12-17 16:57:40 +0530

    features added

--- dictionary/src/Components/Card.jsx
+++ dictionary/src/Components/Card.jsx
@@ -1,19 +1,20 @@
 import { Box, Flex, Heading, Text } from '@chakra-ui/react'
 import React from 'react'
+import SynAnt from './SynAnt'
 
 const Card = ({ antonyms, definitions, partOfSpeech, synonyms }) => {
-    console.log(antonyms, definitions, partOfSpeech, synonyms)
-    let max = 2
   return (
-      <Flex border={"2px solid"} p="1rem" direction={"column"}>
-          <Heading fontSize={'2xl'} fontWeight="md" fontFamily={"cursive"} fontStyle="italic">{partOfSpeech} :-</Heading>
+      <Flex border={"1px solid"} p="1rem" direction={"column"}>
+          <Heading borderBottom={"1px solid"} w="fit-content" fontSize={'2xl'} fontWeight="md" fontFamily={"cursive"} fontStyle="italic">{partOfSpeech} :-</Heading>
           <Flex direction={"column"} gap='.5rem' mt='1rem'>
               {
                   definitions.map((item, i) => {
-                      if (i <= max) return <Text key={i} fontSize={"xl"}>{ i+1}  Ex :{ item.definition}</Text>
+                      if (i <= 2) return <Text key={i} fontSize={"xl"}>{ i+1}  Ex :{ item.definition}</Text>
               })
           }
           </Flex>
+          {synonyms.length > 0 && <SynAnt data={synonyms } role="synonyms"/>}
+          {antonyms.length>0 && <SynAnt data={antonyms} role="antonyms"/>}
           
         </Flex>
   )
